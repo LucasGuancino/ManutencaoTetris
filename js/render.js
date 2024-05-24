84f0f4d52b5bd2611451f937eb1b9217d27f3989
venassi <v.venassigustavo@gmail.com>
2024-05-24 18:34:39 -0300

    refatoracao atividade6

--- js/render.js
+++ js/render.js
@@ -10,27 +10,37 @@ function drawBlock( x, y ) {
 }
 
 // draws the board and the moving shape
-function render() {
-    ctx.clearRect( 0, 0, W, H );
-
+function renderBoard() {
     ctx.strokeStyle = 'black';
-    for ( var x = 0; x < COLS; ++x ) {
-        for ( var y = 0; y < ROWS; ++y ) {
-            if ( board[ y ][ x ] ) {
-                ctx.fillStyle = colors[ board[ y ][ x ] - 1 ];
-                drawBlock( x, y );
+    for (var x = 0; x < COLS; ++x) {
+        for (var y = 0; y < ROWS; ++y) {
+            if (board[y][x]) {
+                ctx.fillStyle = colors[board[y][x] - 1];
+                drawBlock(x, y);
             }
         }
     }
+}
 
+function renderCurrentPiece() {
     ctx.fillStyle = 'red';
     ctx.strokeStyle = 'black';
-    for ( var y = 0; y < 4; ++y ) {
-        for ( var x = 0; x < 4; ++x ) {
-            if ( current[ y ][ x ] ) {
-                ctx.fillStyle = colors[ current[ y ][ x ] - 1 ];
-                drawBlock( currentX + x, currentY + y );
+    for (var y = 0; y < 4; ++y) {
+        for (var x = 0; x < 4; ++x) {
+            if (current[y][x]) {
+                ctx.fillStyle = colors[current[y][x] - 1];
+                drawBlock(currentX + x, currentY + y);
             }
         }
     }
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, W, H);
+}
+
+function render() {
+    clearCanvas();
+    renderBoard();
+    renderCurrentPiece();
 }
